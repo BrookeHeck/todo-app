4b130273eddc073a47201da82a656bbcbf6e76b4
Brooke Heck <b19heck@gmail.com>
2022-11-03 02:56:43 +0000

    add auth provider

---
+++ src/context/auth.js
@@ -0,0 +1,63 @@
+import { useState, createContext, useEffect } from 'react';
+import React from 'react';
+import cookie from 'react-cookies';
+import superagent from 'superagent';
+
+
+export const LoginContext = createContext();
+
+function LoginProvider() {
+
+  const [ loggedIn, setLoggedIn ] = useState(false);
+  const [ token, setToken ] = useState('');
+  const [ user, setUser ] = useState({});
+
+  async function signup(username, password) {
+    const response = await superagent
+      .post(`${process.env.REACT_APP_SERVER_URL}/signup`)
+      .send({
+        'username': username,
+        'password': password,
+      })
+      .catch(console.error);
+      console.log(response);
+  }
+
+  async function login(username, password) {
+    
+  }
+
+  function setLoginState(loggedIn, token, user) {
+    cookie.save('auth', token);
+    setLoggedIn(loggedIn);
+    setToken(token);
+    setUser(user);
+  }
+
+  const logout = () => {
+    setLoginState(false, null, {});
+  };
+
+
+
+  // componentDidMount() {
+  //   const qs = new URLSearchParams(window.location.search);
+  //   const cookieToken = cookie.load('auth');
+  //   const token = qs.get('token') || cookieToken || null;
+  //   this.validateToken(token);
+  // }
+
+  return (
+    <LoginContext.Provider value={{
+      loggedIn: loggedIn,
+      token: token,
+      user: user,
+      login: login,
+      logout: logout,
+    }}>
+      {this.props.children}
+    </LoginContext.Provider>
+  );
+}
+
+export default LoginProvider;
